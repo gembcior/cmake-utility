0683f4c387d813c73ca8281929f61d86b9065e77
Gembcior <gembcior@gmail.com>
2021-12-09 23:55:43 +0100

    Better target suffix handling

--- modules/Utils.cmake
+++ modules/Utils.cmake
@@ -23,31 +23,40 @@ function(TARGET_INSTALL TARGET)
 endfunction()
 
 
-function(GENERATE_TARGET_LISTING TARGET)
+macro(GET_TARGET_OUTPUT_FILE TARGET)
   get_target_property(TARGET_SUFFIX ${TARGET} SUFFIX)
+  set(TARGET_OUTPUT_FILE ${TARGET})
+  if (${TARGET_SUFFIX})
+    string(APPEND ${TARGET_OUTPUT_FILE} ${TARGET_SUFFIX})
+  endif()
+endmacro()
+
+
+function(GENERATE_TARGET_LISTING TARGET)
+  get_target_output_file(${TARGET})
   add_custom_command(TARGET ${TARGET}
     POST_BUILD
-    COMMAND ${TOOLCHAIN_OBJDUMP} -h -D -C ${TARGET}${TARGET_SUFFIX} > ${TARGET}.lst
+    COMMAND ${TOOLCHAIN_OBJDUMP} -h -D -C ${TARGET_OUTPUT_FILE} > ${TARGET}.lst
   )
   file_install(${TARGET} ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.lst)
 endfunction()
 
 
 function(GENERATE_TARGET_HEX_FILE TARGET)
-  get_target_property(TARGET_SUFFIX ${TARGET} SUFFIX)
+  get_target_output_file(${TARGET})
   add_custom_command(TARGET ${TARGET}
     POST_BUILD
-    COMMAND ${TOOLCHAIN_OBJCOPY} -O ihex ${TARGET}${TARGET_SUFFIX} ${TARGET}.hex
+    COMMAND ${TOOLCHAIN_OBJCOPY} -O ihex ${TARGET_OUTPUT_FILE} ${TARGET}.hex
   )
   file_install(${TARGET} ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.hex)
 endfunction()
 
 
 function(GENERATE_TARGET_BIN_FILE TARGET)
-  get_target_property(TARGET_SUFFIX ${TARGET} SUFFIX)
+  get_target_output_file(${TARGET})
   add_custom_command(TARGET ${TARGET}
     POST_BUILD
-    COMMAND ${TOOLCHAIN_OBJCOPY} -O binary ${TARGET}${TARGET_SUFFIX} ${TARGET}.bin
+    COMMAND ${TOOLCHAIN_OBJCOPY} -O binary ${TARGET_OUTPUT_FILE} ${TARGET}.bin
   )
   file_install(${TARGET} ${CMAKE_CURRENT_BINARY_DIR}/${TARGET}.bin)
 endfunction()
@@ -63,10 +72,10 @@ endfunction()
 
 
 function(PRINT_TARGET_SIZE TARGET)
-  get_target_property(TARGET_SUFFIX ${TARGET} SUFFIX)
+  get_target_output_file(${TARGET})
   add_custom_command(TARGET ${TARGET}
     POST_BUILD COMMAND
-    ${TOOLCHAIN_SIZE} ${TARGET}${TARGET_SUFFIX}
+    ${TOOLCHAIN_SIZE} ${TARGET_OUTPUT_FILE}
     COMMENT "Size report for ${TARGET}"
   )
 endfunction()
