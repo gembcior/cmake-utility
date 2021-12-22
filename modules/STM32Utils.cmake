b6106549d4c342988a598f31346ec16bc35ddaaa
Gembcior <gembcior@gmail.com>
2021-12-22 19:58:28 +0100

    Fix macro get target output file

--- modules/STM32Utils.cmake
+++ modules/STM32Utils.cmake
@@ -2,7 +2,6 @@ include(Utils)
 
 
 function(STM32_TARGET_APP_RELEASE TARGET)
-  set_property(TARGET ${TARGET} PROPERTY SUFFIX .elf)
   print_target_size(${TARGET})
   generate_target_hex_file(${TARGET})
   generate_target_bin_file(${TARGET})
