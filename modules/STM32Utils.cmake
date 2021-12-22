fbeb25b6a22ff20488cdf089b4ae3c8b02b3ce15
Gembcior <gembcior@gmail.com>
2021-12-22 20:07:00 +0100

    Restore removed line

--- modules/STM32Utils.cmake
+++ modules/STM32Utils.cmake
@@ -2,6 +2,7 @@ include(Utils)
 
 
 function(STM32_TARGET_APP_RELEASE TARGET)
+  set_property(TARGET ${TARGET} PROPERTY SUFFIX .elf)
   print_target_size(${TARGET})
   generate_target_hex_file(${TARGET})
   generate_target_bin_file(${TARGET})
